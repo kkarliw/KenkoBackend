6bc8e18e77150c897da7adfe4c9769cdabcbe855
Karla <amarantojimenezk@gmail.com>
2026-02-21 20:06:00 -0500

    backend kenko

--- update_back.bat
+++ update_back.bat
@@ -1,11 +1,11 @@
 @echo off
 set GIT_PATH=C:\Users\Karla\AppData\Local\Programs\Git\cmd\git.exe
-echo Inicianco actualizacion de Backend...
+echo Inificando actualizacion...
 "%GIT_PATH%" init
 "%GIT_PATH%" remote remove origin
 "%GIT_PATH%" remote add origin https://github.com/kkarliw/KenkoBackend.git
 "%GIT_PATH%" add .
-"%GIT_PATH%" commit -m "Actualizacion Backend - cambios back"
+"%GIT_PATH%" commit -m "Actualizacion Backend cambios back"
 "%GIT_PATH%" branch -M main
-"%GIT_PATH%" push -u origin main
+"%GIT_PATH%" push -f -u origin main
 echo Finalizado.
